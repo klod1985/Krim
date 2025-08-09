331a099b5bdfe38e8f4635c93dd12c786f71c4c2
klod1985 <seed1985113@gmail.com>
2025-08-09 13:26:25 +0300

    Test 1 privet!

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+        import java.util.Scanner;
+
+        import static java.lang.Math.*;
+
+
+        public class Main {
+            public static void main(String[] args) {
+
+                Scanner scan = new Scanner(System.in);
+                int number = scan.nextInt();
+
+
+                for (int a = 1; a <= number; a++){
+                    if (number % a == 0){
+                        System.out.println(a);
+                    }
+                }
+
+
+                    }
+
+                }
+
+
+
