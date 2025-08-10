31d767d014efe6778a8c724fe5a486d474c2abc6
klod1985 <seed1985113@gmail.com>
2025-08-10 22:16:19 +0300

    Test 1 privet!

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,36 @@
         public class Main {
             public static void main(String[] args) {
 
-                Scanner scan = new Scanner(System.in);
-                int number = scan.nextInt();
-                //Тест
+               int Pervoe_Chislo = 10;
+               int Vtoroe_Chislo = 142;
+
+              int MaxChisloDelitel = 0;
+              int chislo = 0;
+
+              for (int peremenaya = Pervoe_Chislo; peremenaya < Vtoroe_Chislo; peremenaya++){
 
 
-                for (int a = 1; a <= number; a++){
-                    if (number % a == 0){
-                        System.out.println(a);
+                  int Deliteley_Seychas = 0;
+
+                  for (int delitel = 1;delitel <= peremenaya;delitel++){
+                      if(peremenaya % delitel == 0){
+                          Deliteley_Seychas++;
+                      }
+
+
+                  }
+                  if (Deliteley_Seychas > MaxChisloDelitel){
+                      MaxChisloDelitel = Deliteley_Seychas;
+                      chislo = peremenaya;
+                  }
+
+              }
+                System.out.println(MaxChisloDelitel);
+                System.out.println(chislo);
+
+
+
                     }
-                }
-
-
-                    }
-
                 }
 
 
